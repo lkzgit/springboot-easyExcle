43f5358a731a7a9299d74eadd40b8ec4d85de4ab
likuozhan <lijia9512@163.com>
2020-10-27 13:38:25 +0800

    简单样式提交

---
+++ src/main/java/com/easyexcle/demo/SpringbootExcleApplication.java
@@ -0,0 +1,13 @@
+package com.easyexcle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExcleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExcleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easyexcle/demo/SpringbootExcleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyexcle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExcleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
